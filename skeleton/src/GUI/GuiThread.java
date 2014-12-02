16ade1b7238ac60e9660f42990cff1122d294232
Oskar holmberg <oskarholmberg@msn.com>
2014-12-02 15:49:40 +0100

    nhj

---
+++ skeleton/src/GUI/GuiThread.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import skeleton.client.ClientMonitor;
+
+public class GuiThread extends Thread {
+
+	ClientMonitor m;
+	GUI gui;
+
+	public GuiThread(ClientMonitor m, GUI gui) {
+		super();
+		this.m = m;
+		this.gui = gui;
+	}
+
+	public void run() {
+		while (true) {
+			gui.refreshImage(m.getLatestImage(), m.cameraInMovie(), m.getCameraNbr());
+		}
+	}
+
+}

eff807a451fd233221bfeb63b52eac7c838dcf66
Oskar holmberg <oskarholmberg@msn.com>
2014-12-03 13:44:27 +0100

    Added radio buttons for mode selection

--- skeleton/src/GUI/GUI.java
+++ skeleton/src/GUI/GUI.java
@@ -26,20 +26,18 @@ import skeleton.client.ClientMonitor;
 import skeleton.client.ClientSocket;
 
 public class GUI extends JFrame implements ItemListener {
-	
+
 	private ClientMonitor m;
 	private ArrayList<ClientSocket> camList;
 	private ArrayList<ImagePanel> imagePanels;
 	private JPanel displayPanel;
-	private JCheckBox movie;
 	private BufferedImage nocamfeed, waitingforconnect;
 	private JCheckBox synch;
 	private JPanel camDisplay;
 	private JLabel[] delays = new JLabel[4];
 	private JTextArea actionLogArea = new JTextArea();
-	private boolean firstCall = true;
 	private byte[] jpeg = new byte[AxisM3006V.IMAGE_BUFFER_SIZE];
-	public static final int MAXCAMERAS = 2; 
+	public static final int MAXCAMERAS = 2;
 
 	public GUI(ClientMonitor m) {
 		super();
@@ -49,22 +47,21 @@ public class GUI extends JFrame implements ItemListener {
 		imagePanels = new ArrayList<ImagePanel>();
 		delays[0] = new JLabel("Camera 1");
 		delays[1] = new JLabel("Camera 2");
-		addMovieCheckbox();
 		addSyncCheckbox();
 		addMenu();
 		camDisplay = new JPanel();
 		camDisplay.setLayout(new GridLayout(0, 2));
 
 		displayPanel = new JPanel();
-		JPanel checkBoxPanel = new JPanel();
+		JPanel modeSelectionPanel = new JPanel();
 		displayPanel.setLayout(new GridLayout(0, 2));
-		checkBoxPanel.setLayout(new BorderLayout());
-		checkBoxPanel.add(movie, BorderLayout.WEST);
-		checkBoxPanel.add(synch, BorderLayout.EAST);
+		modeSelectionPanel.setLayout(new BorderLayout());
+		modeSelectionPanel.add(new CameraModeRadioButtonPane(this, m), BorderLayout.WEST);
+		modeSelectionPanel.add(synch, BorderLayout.EAST);
 		displayPanel.add(delays[0]);
 		displayPanel.add(delays[1]);
 		displayPanel.add(actionLogArea);
-		displayPanel.add(checkBoxPanel);
+		displayPanel.add(modeSelectionPanel);
 		try {
 			nocamfeed = ImageIO.read(new File("../files/nocamfeed.jpg"));
 			waitingforconnect = ImageIO
@@ -106,22 +103,10 @@ public class GUI extends JFrame implements ItemListener {
 
 		Object source = e.getItemSelectable();
 
-		if (source == movie) {
-			if (e.getStateChange() == ItemEvent.SELECTED) {
-				m.uppdateMovieMode(true);
-				delays[0].setText("Camera 1: Movie mode active.");
-				delays[1].setText("Camera 2: Movie mode active.");
-			} else if (e.getStateChange() == ItemEvent.DESELECTED)
-				m.uppdateMovieMode(false);
-			delays[0].setText("Camera 1: Movie mode inactive.");
-			delays[1].setText("Camera 2: Movie mode inactive.");
-
-		} else if (source == synch) {
-			if (e.getStateChange() == ItemEvent.SELECTED)
-				m.uppdateSynchMode(true);
-			else if (e.getStateChange() == ItemEvent.DESELECTED)
-				m.uppdateSynchMode(false);
-		}
+		if (e.getStateChange() == ItemEvent.SELECTED)
+			m.uppdateSynchMode(true);
+		else if (e.getStateChange() == ItemEvent.DESELECTED)
+			m.uppdateSynchMode(false);
 
 	}
 
@@ -137,13 +122,6 @@ public class GUI extends JFrame implements ItemListener {
 		add(menuBar, BorderLayout.NORTH);
 	}
 
-	private void addMovieCheckbox() {
-		movie = new JCheckBox("Force movie mode");
-		movie.setMnemonic(KeyEvent.VK_M);
-		movie.setSelected(false);
-		movie.addItemListener(this);
-	}
-
 	private void addSyncCheckbox() {
 		synch = new JCheckBox("Synchronized mode");
 		synch.setMnemonic(KeyEvent.VK_S);
@@ -152,7 +130,7 @@ public class GUI extends JFrame implements ItemListener {
 	}
 
 	public void addCamera() {
-		ImagePanel newCam = new ImagePanel(camList.size()-1);
+		ImagePanel newCam = new ImagePanel(camList.size() - 1);
 		imagePanels.add(newCam);
 		camDisplay.remove(camList.size() - 1);
 		camDisplay.add(newCam, camList.size() - 1);

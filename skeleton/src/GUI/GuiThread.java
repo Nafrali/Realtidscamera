627318a4c2089b1d27c349e366373c696c912dc9
Erik Munkby <dat12emu@varg-6.student.lth.se>
2014-12-03 13:03:05 +0100

    TravelTime Fixed

--- skeleton/src/GUI/GuiThread.java
+++ skeleton/src/GUI/GuiThread.java
@@ -1,11 +1,13 @@
 package GUI;
 
 import skeleton.client.ClientMonitor;
+import skeleton.client.ImageClass;
 
 public class GuiThread extends Thread {
 
 	ClientMonitor m;
 	GUI gui;
+	private ImageClass currentImage;
 
 	public GuiThread(ClientMonitor m, GUI gui) {
 		super();
@@ -15,7 +17,9 @@ public class GuiThread extends Thread {
 
 	public void run() {
 		while (true) {
-			gui.refreshImage(m.getLatestImage().getImage(), m.cameraInMovie(), m.getCameraNbr());
+			currentImage = m.getLatestImage();
+			System.out.println(currentImage.getTravelTime());
+			gui.refreshImage(currentImage.getImage(), m.cameraInMovie(), m.getCameraNbr());
 //			gui.refreshImage(m.getLatestImage(), m.cameraInMovie(), m.getCameraNbr());
 		}
 	}
